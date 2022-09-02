d65b491b4297659ad10122191ff072959fc4826c
stacy  Goldstein <stacygoldstein@gmail.com>
2022-09-02 22:24:31 +0000

    maybe a working version of nim?

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
 import random
 
-stones = 12;
-while (stones >0):
-  stonesTaken = input("How many stones would you like to take? (Choose 1,2 or 3 only)")
-  print("You have taken " + stonesTaken + " stones")
-  stonesTaken = int(stonesTaken)
-  stones = stones - stonesTaken
-  stones = str(stones)
-  print (stones + " stones are left")
-  stones = int(stones)
-  if stones == 0:
-    print("You took the last stone.You win!")
-  elif stones >3:
-    stonesTaken = random.randint(1,4)
-    print("The computer has taken " + str(stonesTaken) + " stones")
-    # print(stonesTaken)
-    # print("stones")
-    stones = stones - stonesTaken
-    print("There are " + str(stones) + " left in the pot")
-    # print(stones)
-    # print("left in the pot")
-
-  else:
+stones = 12
+while (int(stones) >0):
+  stonesTakenHuman = input("How many stones would you like to take? (Choose 1,2 or 3 only)")
+  print("You have taken " + stonesTakenHuman + " stones")
+  stones -= int(stonesTakenHuman)
  
-   stones = stones - stonesTaken
-   print("The machine took " + str(stonesTaken) + ". There are " + str(stones)+ " left")
-  
+  stonesTakenComp = random.randint(1,3)
+  print("The computer has taken" +           
+  str(stonesTakenComp) + " stones")
+  stones -= stonesTakenComp
+  print (str(stones) + " stones are left") 
 
-  if stones <=0:
-    print("The machine took the last stone. You lost!")
+  if (int(stones)-int(stonesTakenComp) <=0):
+
+    print("The machine took the last stone. You       lost!")
+
+  elif(int(stones)-int(stonesTakenHuman) ==0):
+    print("You took the last stone you win!")
+
+   # elif stones >3:
+  
+  #   # print(stonesTaken)
+  #   # print("stones")
+  #   stones = stones - stonesTakenComp
+  #   print("There are " + str(stones) + " left in the pot")
+  #   # print(stones)
+  #   # print("left in the pot")
+
+  # else:
+ 
+  #  stones = stones - stonesTaken
+  #  print("The machine took " + str(stonesTaken) + ". There are " + str(stones)+ " left") 
 
       
-  stones= int(stones)
-
-#print("You have no more stones")
-   
-#need to add in random number generator to play against computer 
-#need else/if 
+  
