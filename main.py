9c064df4045e9c4aee897cf2192e2b24d4f12856
stacy  Goldstein <stacygoldstein@gmail.com>
2022-09-02 22:57:46 +0000

    this should work!

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ while (int(stones) >0):
   stonesTakenHuman = input("How many stones would you like to take? (Choose 1,2 or 3 only)")
   print("You have taken " + stonesTakenHuman + " stones")
   stones -= int(stonesTakenHuman)
- 
+  print("There are " + str(stones) + " left")
+  if (int(stones)==0):
+    print("You win! You took the last stone")
+    break
   stonesTakenComp = random.randint(1,3)
-  print("The computer has taken" +           
+  print("The computer has taken " +           
   str(stonesTakenComp) + " stones")
   stones -= stonesTakenComp
-  print (str(stones) + " stones are left") 
+  print (str(stones) + " stones are left")       
+  if(int(stones) <=0):
+    print("Sorry the computer won :(")
+    break
 
-  if (int(stones)-int(stonesTakenComp) <=0):
-
-    print("The machine took the last stone. You       lost!")
-
-  elif(int(stones)-int(stonesTakenHuman) ==0):
-    print("You took the last stone you win!")
+  
 
    # elif stones >3:
   
